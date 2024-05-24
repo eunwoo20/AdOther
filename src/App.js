80efcffdd3c6989da832cc26b94dd885dc3906e2
Utkarsha Jadhav <jadhavutkarsha6@gmail.com>
2024-05-24 21:51:22 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Faq from "./Faq";
 import ContactUs from "./ContactUs";
 import SignUp from "./SignUp";
-import LogIn from './LogIn';
+import LogIn from "./LogIn";
+
 
 function App() {
   return (
-    <div>
-          
-       <LogIn/>                                         
-    </div>
+
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Faq />}>
+          <Route index element={<ContactUs />} />
+          <Route path="signup" element={<SignUp />} />
+          <Route path="login" element={<LogIn />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+   
   );
 }
 
-export default App;
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
 //<ContactUs/>
    //<Faq/>
   // <SignUp/>  
